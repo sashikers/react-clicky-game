6dcdb02e0957d5d2fc4f30b2149c467c1fb0d7d9
Sasha <sashikers@gmail.com>
2018-04-06 09:36:18 -0700

    basic page set up

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <div className="nav-wrapper z-depth-3">
+            <div className="brand-logo center">Clicky game!</div>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li className="navButton">Score</li>
+              <li className="navButton">Top score</li>
+              <li className="navButton">Games played</li>
+            </ul>
+          </div>
+        </nav>
+        <div className="container header">
+          <p>Start the game by clicking on the images!<br/>
+            BUT! Click each image only once - clicking the same image starts the game over!</p>        
+        </div>
       </div>
     );
   }
